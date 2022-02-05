b325cc83051a1d12e9d7fddb1e7f7665c41a744e
Yasir KARA <y_a_s_i_r95@hotmail.com>
2022-02-05 13:20:00 +0300

    added methods for users.service

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -10,11 +10,36 @@ export class UsersService {
   constructor(@InjectRepository(User) private repo: Repository<User> ){ }
 
   create(email: string, password: string ){
-
     const user = this.repo.create({email, password});
-
     return this.repo.save(user);
+  }
 
+  findOne(id:number){
+    return this.repo.findOne(id)
+  }
+
+  find(email:string){
+    return this.repo.find({email});
+  }
+
+  // Partial<User>  ya email i ya da passwordu yada ikisinide update etmek
+  //istersek bu gösterim sorunu çözüyor(User entity için)
+  async update(id: number, attrs: Partial<User> ){
+    const user = await this.findOne(id);
+    if(!user){
+      throw new Error("user not found");
+    }
+    Object.assign(user,attrs);
+    return this.repo.save(user);
+  }
+
+  //remove for Entiteis , delete for ids
+  async remove(id: number) {
+    const user = await this.findOne(id);
+    if(!user){
+      throw new Error("already removed!!");
+    }
+    return this.repo.remove(user);
   }
 
 }
