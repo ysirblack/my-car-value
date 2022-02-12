68953567142db31ba648e56b71e2ce75e0f0a8d3
Yasir Kara <yasir.kara@hotmail.com>
2022-02-12 12:55:27 +0300

    auth test

--- src/users/auth.service.spec.ts
+++ src/users/auth.service.spec.ts
@@ -9,10 +9,21 @@ describe('AuthService', () => {
 
   beforeEach(async () => {
     // Create a fake copy of the users service
+    const users: User[] = [];
     fakeUsersService = {
-      find: () => Promise.resolve([]),
-      create: (email: string, password: string) =>
-        Promise.resolve({ id: 1, email, password } as User),
+      find: (email: string) => {
+        const filteredUsers = users.filter((user) => user.email === email);
+        return Promise.resolve(filteredUsers);
+      },
+      create: (email: string, password: string) => {
+        const user = {
+          id: Math.floor(Math.random() * 999999),
+          email,
+          password,
+        } as User;
+        users.push(user);
+        return Promise.resolve(user);
+      },
     };
 
     const module = await Test.createTestingModule({
@@ -42,8 +53,7 @@ describe('AuthService', () => {
   });
 
   it('throws an error if user signs up with email that is in use', async (done) => {
-    fakeUsersService.find = () =>
-      Promise.resolve([{ id: 1, email: 'a', password: '1' } as User]);
+    await service.signup('asdf@asdf.com', 'asdf');
     try {
       await service.signup('asdf@asdf.com', 'asdf');
     } catch (err) {
@@ -60,30 +70,18 @@ describe('AuthService', () => {
   });
 
   it('throws if an invalid password is provided', async (done) => {
-    fakeUsersService.find = () =>
-      Promise.resolve([
-        { email: 'asdf@asdf.com', password: 'laskdjf' } as User,
-      ]);
+    await service.signup('laskdjf@alskdfj.com', 'password');
     try {
-      await service.signin('laskdjf@alskdfj.com', 'passowrd');
+      await service.signin('laskdjf@alskdfj.com', 'laksdlfkj');
     } catch (err) {
       done();
     }
   });
 
   it('returns a user if correct password is provided', async () => {
-    fakeUsersService.find = () =>
-      Promise.resolve([
-        {
-          email: 'asdf@asdf.com',
-          password:
-            '1f3f1f78b727fa56.af7186ab0d0a8e9cf1ae2c5c179784947a2fe349d3a3e82f6422c0d7cbc2f925',
-        } as User,
-      ]);
+    await service.signup('asdf@asdf.com', 'mypassword');
 
     const user = await service.signin('asdf@asdf.com', 'mypassword');
     expect(user).toBeDefined();
-
-    
   });
 });
